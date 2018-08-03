65b4a07299c0092ee851d0e3226f8fed5a9473db
SebastienWauquier <sebastien.wauquier0@gmail.com>
2018-08-03 22:15:18 +0200

    add standard assets

---
+++ TestProjection/Assets/RockSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSound : MonoBehaviour {
+
+    public bool PlaySound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (PlaySound)
+        {
+            if (!this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("rockturnloop"))
+            {
+                this.GetComponent<AudioSource>().Play();
+
+                PlaySound = false;
+            }
+        }
+	}
+}
