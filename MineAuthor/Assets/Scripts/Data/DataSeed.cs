26db5384de76963ecfc3841059c2c7ab8c4f901b
Srevnel <srev.spa@outlook.be>
2018-08-04 23:14:42 +0200

    datamap up

---
+++ MineAuthor/Assets/Scripts/Data/DataSeed.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Data
+{
+    public class DataSeed
+    {
+        public static int[][] getMap(int width, int height, List<Position> positions)
+        {
+            int[][] map = new int[height][];
+            for (int i = 0; i < height; i++)
+            {
+                map[i] = new int[width];
+            }
+
+
+            foreach (Position position in positions)
+            {
+                map[position.Y][position.X] = 1;
+            }
+
+
+            return map;
+        }
+
+    }
+}
