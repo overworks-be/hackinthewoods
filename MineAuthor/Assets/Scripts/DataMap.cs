a6b78aaa7cf8c336d52dedffbb52f46361d7670c
Srevnel <srev.spa@outlook.be>
2018-08-04 14:02:49 +0200

    add game logic scripts

---
+++ MineAuthor/Assets/Scripts/DataMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    class DataMap
+    {
+       public static int[][] map =
+    {
+    new int[] {1,0,1,0,1},
+    new int[] {0,0,1,0,0},
+    new int[] {0,1,1,1,0},
+    new int[] {0,0,0,0,0},
+    new int[] {1,0,1,0,1},
+    new int[] {0,0,1,0,0}
+};
+    }
+}
