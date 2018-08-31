c9bbd66e63b8f77d63df8f070b99c978431d1cca
= <=>
2018-08-31 15:34:36 +0200

    change mine coordinates

--- MineAuthor/Assets/Scripts/GameEngine.cs
+++ MineAuthor/Assets/Scripts/GameEngine.cs
@@ -25,31 +25,31 @@ public class GameEngine : MonoBehaviour
     {
 
         List<Coordinates> mineList = new List<Coordinates>();
-        mineList.Add(new Coordinates(7, 32));
-        mineList.Add(new Coordinates(9, 30));
-        mineList.Add(new Coordinates(12, 28));
-        mineList.Add(new Coordinates(10, 26));
-        mineList.Add(new Coordinates(6, 24));
-        mineList.Add(new Coordinates(11, 23));
-        mineList.Add(new Coordinates(14, 23));
-        mineList.Add(new Coordinates(9, 21));
-        mineList.Add(new Coordinates(5, 22));
-        mineList.Add(new Coordinates(13, 22));
-        mineList.Add(new Coordinates(10, 20));
-        mineList.Add(new Coordinates(8, 18));
-        mineList.Add(new Coordinates(10, 15));
-        mineList.Add(new Coordinates(13, 12));
-        mineList.Add(new Coordinates(16, 11));
-        mineList.Add(new Coordinates(10, 9));
-        mineList.Add(new Coordinates(7, 6));
-        mineList.Add(new Coordinates(14, 4));
+        mineList.Add(new Coordinates(6,4));
+        mineList.Add(new Coordinates(9, 7));
+        mineList.Add(new Coordinates(13, 2));
+        mineList.Add(new Coordinates(12,10 ));
+        mineList.Add(new Coordinates(15,9 ));
+        mineList.Add(new Coordinates(9, 13));
+        mineList.Add(new Coordinates(7,16 ));
+        mineList.Add(new Coordinates(9, 18));
+        mineList.Add(new Coordinates(4, 20));
+        mineList.Add(new Coordinates(12,20 ));
+        mineList.Add(new Coordinates(8,21 ));
+        mineList.Add(new Coordinates(13,23 ));
+        mineList.Add(new Coordinates(10, 23));
+        mineList.Add(new Coordinates(5, 24));
+        mineList.Add(new Coordinates(9,26));
+        mineList.Add(new Coordinates(11, 28));
+        mineList.Add(new Coordinates(8, 30));
+        mineList.Add(new Coordinates(6,32 ));
 
         List<Coordinates> targetZone = new List<Coordinates>();
+        targetZone.Add(new Coordinates(10, 32));
         targetZone.Add(new Coordinates(11, 32));
-        targetZone.Add(new Coordinates(12, 32));
 
         this.gameService = new GameService();
-        gameService.startNewGame(20, 45, mineList, new List<Coordinates>(), targetZone, 32);
+        gameService.startNewGame(20, 45, mineList, new List<Coordinates>(), targetZone, 0);
 
 
  
@@ -99,31 +99,7 @@ public class GameEngine : MonoBehaviour
 
     public bool isCellRevealed(float x, float y)
     {
-<<<<<<< HEAD
-        int x = (int)xF;
-        int y = (int)yF;
-
-        if (grid.Cells[y][x].IsBomb)
-        {
-            Debug.Log("BOUM!");
-            gameOver("Mine Detonation");
-            return -1;
-        }
-        else
-        {
-            //Debug.Log("near bomb: " + grid.Cells[y][x].AdjacentBomb);
-            if(grid.Cells[y][x].AdjacentBomb == -2)
-            {
-
-                Debug.Log("Time: " + timer);
-            }
-
-
-            return grid.Cells[y][x].AdjacentBomb;
-        }
-=======
         return this.getCellState(x, y) == CellState.Revealed;
->>>>>>> 3a8263cbe9acf65405bf6d9c52cf2d1c48288904
     }
 
 }
